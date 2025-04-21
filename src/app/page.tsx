9601c668aab6b9b2c54892779143a2286415610d
KaosProjectVE <kaoscreativestudiove@gmail.com>
2025-04-20 21:43:02 -0400

    Ajustes en About con partículas hexagonales y corrección de imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // src/app/page.tsx
 
-import Hero from '@/Components/Hero'
-import Services from '@/Components/services'
+import Hero from '@/components/Hero'
+import Services from '@/components/services'
 import PortfolioGrid from '@/components/PortfolioGrid'
 import Abaut from '@/app/about/page'
 export default function HomePage() {
